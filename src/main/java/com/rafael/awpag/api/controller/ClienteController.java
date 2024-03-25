636d84bd86eae79b5b58ec07c2483d43c4b434f2
rafaelkenedy <k.rafaelalves@gmail.com>
2024-03-25 00:03:08 -0300

    cliente crud

--- src/main/java/com/rafael/awpag/api/controller/ClienteController.java
+++ src/main/java/com/rafael/awpag/api/controller/ClienteController.java
@@ -1,23 +1,73 @@
 package com.rafael.awpag.api.controller;
 
 import com.rafael.awpag.domain.model.Cliente;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import com.rafael.awpag.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ClienteRepository repository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> getClientes() {
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return repository.findAll();
     }
 
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long clienteId) {
+        Optional<Cliente> cliente = repository.findById(clienteId);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Cliente addCliente(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        if (!repository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        cliente = repository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> removeCliente(@PathVariable Long clienteId) {
+        if (!repository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/rafael/awpag/domain/model/Cliente.java
+++ src/main/java/com/rafael/awpag/domain/model/Cliente.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,10 +16,9 @@ public class Cliente {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
-    @Column(name = "fone")
     private String telefone;
 }
