9c7887adaed9fef8df48a59f5725fa7cb9c196d6
rafaelkenedy <k.rafaelalves@gmail.com>
2024-03-24 22:26:20 -0300

    database added

--- src/main/java/com/rafael/awpag/api/controller/ClienteController.java
+++ src/main/java/com/rafael/awpag/api/controller/ClienteController.java
@@ -1,26 +1,23 @@
 package com.rafael.awpag.api.controller;
 
 import com.rafael.awpag.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static java.util.Collections.singletonList;
-
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> getClientes() {
-        var cliente = Cliente.builder()
-                .id(1L)
-                .nome("Rafael")
-                .telefone("+55 11 99999-9999")
-                .email("rafael@rafael.com")
-                .build();
-
-        return singletonList(cliente);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 
 }
--- src/main/java/com/rafael/awpag/domain/model/Cliente.java
+++ src/main/java/com/rafael/awpag/domain/model/Cliente.java
@@ -1,16 +1,26 @@
 package com.rafael.awpag.domain.model;
 
-import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
     private String email;
+    @Column(name = "fone")
     private String telefone;
 }
